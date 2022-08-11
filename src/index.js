27f2cbe1bba1b010b1aaf116ae9ed056d32e1775
Dmitry <Reismoos@gmail.om>
2022-08-11 19:41:58 +0300

    complite task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  /* let openArr = matrix.flat(Infinity).sort(function(a, b){return a-b}); */
+if (!Array.isArray(matrix)){
+  return []
+} else if (!matrix[0]) {
+  return []
+}
+  let result=[];
+  for (let i in matrix){
+    if (i % 2 === 0){
+      matrix[i].forEach(el => {result.push(el)        
+      });
+    } else {
+      let r = matrix[i].reverse()
+      r.forEach(el => {result.push(el)        
+      });
+    }
+  }
+  return result
 }
